6bf749755fd0ee58da031e7b15b8fe1868fbdd41
Pavan Koushik <pavankoushik.kommuri@sjsu.edu>
2022-11-29 18:18:19 -0800

    add login frontend design

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React,{ useState }  from 'react';
+import {
+	Container,
+	Button,
+	Grid,
+	Paper,
+	TextField,
+	IconButton,
+	InputAdornment,
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
 
 function Login(){
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        showPass: false,
+    });
+    const handlePassVisibility = ()=>{
+        setValues(
+            {
+                ...values,
+                showPass: !values.showPass
+            }
+        )
+    };
+
     return (
         <div>
-            This is the Login page
+            <Container maxWidth="sm">
+<Grid
+	container
+	spacing={2}
+	direction="column"
+	justifyContent="center"
+	style={{ minHeight: "100vh" }}
+>
+<Paper elelvation={2} sx={{ padding: 5 }}>
+<form>
+<Grid container direction="column" spacing={2}>
+	<Grid item>
+		<TextField
+			type="email"
+			fullWidth
+			label="Enter your email"
+			placeholder="Email Address"
+			variant="outlined"
+			required
+		/>
+	</Grid>
+
+	<Grid item>
+	<TextField
+		type={values.showPass ? "text" : "password"}
+		fullWidth
+		label="Password"
+		placeholder="Password"
+		variant="outlined"
+		required
+		InputProps={{
+			endAdornment: (
+				<InputAdornment position="end">
+					<IconButton
+						onClick={handlePassVisibility}
+						aria-label="toggle password"
+						edge="end"
+					>
+						{values.showPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+					</IconButton>
+				</InputAdornment>
+			),
+		}}
+	/>
+	</Grid>
+
+	<Grid item>
+	<Button type="submit" fullWidth variant="contained">
+		Sign In
+	</Button>
+	</Grid>
+</Grid>
+</form>
+</Paper>
+</Grid>
+</Container>
         </div>
     )
 }
