cde47a8288ab20b06fdad91634b5772eb0c18693
Neeharika Kandikattu <neeharika.kandikattu@sjsu.edu>
2022-12-01 22:04:14 -0800

    config changes

--- client/src/components/DateComponent.jsx
+++ client/src/components/DateComponent.jsx
@@ -5,12 +5,13 @@ import { Link } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import {useNavigate} from 'react-router-dom';
-import Axios from 'axios'
+import Axios from 'axios';
+var config = require('../../../server/configs/Config.js');
 
 export const DateComponent = ({person}) => {
   let navigate = useNavigate();
   useEffect(()=>{
-		Axios.get("http://localhost:8888/isLoggedIn", {
+		Axios.get(`http://localhost:${config.serverPort}/isLoggedIn`, {
 			headers:{
 				"x-access-token": localStorage.getItem("token")
 			}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,10 +14,12 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Axios from 'axios'
 import {useNavigate} from 'react-router-dom';
 
+var config = require('../../../server/configs/Config.js');
+
 
 function Login(){
 	useEffect(()=>{
-		Axios.get("http://localhost:8888/isLoggedIn", {
+		Axios.get(`http://localhost:${config.serverPort}/isLoggedIn`, {
 			headers:{
 				"x-access-token": localStorage.getItem("token")
 			}
@@ -39,7 +41,7 @@ function Login(){
         showPass: false,
     });
     const checkCreds = ()=>{
-        Axios.post("http://localhost:8888/login", {email: email, password: password})
+        Axios.post(`http://localhost:${config.serverPort}/login`, {email: email, password: password})
         .then((response)=>{
             console.log(response.data);
             if(response.data.auth){
